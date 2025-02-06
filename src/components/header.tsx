5743187e09d3f621f37bd15a147d692d14923210
Rodolpho Reis <rodolphoreis@live.com>
2025-02-06 17:15:28 +0000

    feat: update Header component layout and styling for improved visibility

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,19 +4,18 @@ import { ShoppingCartIcon } from "lucide-react";
 
 const Header = () => {
   return (
-    <div className="w-full h-20 ">
+    <div className="w-full h-32 ">
       <div className="max-w-[1200px] mx-auto h-full flex items-center justify-between">
-        <div className="flex items-center gap-1">
-          <ShoppingCartIcon className="h-12 w-10 text-blue-400 font-bold" />
-          <div className="flex flex-col gap-0">
-            <div>
-              <span className="font-medium text-2xl text-blue-400">ON</span>
-              <span className="font-medium text-2xl text-orange-300">Line</span>
-            </div>
-            <span className="text-xs font-medium text-zinc-400 mt-[-5px]">
-              Shop
-            </span>
+        <div className="flex flex-col items-center gap-1">
+          <div className="flex items-center gap-1">
+            <ShoppingCartIcon className="h-8 w-8 text-orange-300 font-bold" />
+
+            <span className="font-medium text-2xl text-blue-400">Dev</span>
           </div>
+
+          <span className="text-sm font-medium text-zinc-400 mt-[-5px]">
+            Shop Online
+          </span>
         </div>
         <div>
           <ModeToggle />
