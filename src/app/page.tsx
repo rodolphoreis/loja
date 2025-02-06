97418bff89fe27736882c9925cb7438c081dd1e2
Rodolpho Reis <rodolphoreis@live.com>
2025-02-06 16:14:10 +0000

    feat: refactor Home component and add Product and Success pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import React from "react";
+
+const Home = () => {
   return (
-    <>
-      <h1>Loja online</h1>
-    </>
+    <div>
+      <h1>Home</h1>
+    </div>
   );
-}
+};
+
+export default Home;
