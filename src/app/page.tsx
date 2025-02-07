61d245aae907c23582265f73b8af957afa6b31de
Rodolpho Reis <rodolphoreis@live.com>
2025-02-07 19:19:46 +0000

    feat: implement Carousel component in Home page for enhanced visual display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="w-full h-screen ">
+      <div className="w-full max-w-[1200px] mx-auto h-screen items-center flex justify-center ">
+        <Carousel className="mt-[-80px] flex mx-auto w-[80%] h-full items-center justify-center ">
+          <CarouselContent className=" flex w-full h-full">
+            <CarouselItem className=" flex justify-center">...</CarouselItem>
+            <CarouselItem>...</CarouselItem>
+            <CarouselItem>...</CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </div>
   );
 };
